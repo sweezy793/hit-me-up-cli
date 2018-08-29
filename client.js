7b0c25628a9cd339fbe4cfdbe36f5c6cef08ff21
sweezy793 <sarthaks793@gmail.com>
2018-08-30 01:25:47 +0530

    Banner added, prompt works

--- client.js
+++ client.js
@@ -1,6 +1,11 @@
 const {ChatManager, TokenProvider} = require('@pusher/chatkit');
 const {JSDOM} = require('jsdom');
 const util = require('util');
+const prompt=require('prompt');
+const chalk       = require('chalk');
+const clear       = require('clear');
+const figlet      = require('figlet');
+
 
 const makeChatkitNodeCompatible = () => {  //chatkit-client works properly in browser but not node. makeChatkitNodeCompatible makes it work the same as it does in browser
     const {window} = new JSDOM();
@@ -10,9 +15,29 @@ const makeChatkitNodeCompatible = () => {  //chatkit-client works properly in br
 
 makeChatkitNodeCompatible();
 
+clear();
+console.log(
+  chalk.whiteBright.bgRed.bold(
+    figlet.textSync('Hit Me Up!', { horizontalLayout: 'full' })
+  )
+);
+
 const main = async () => {
     try {
-      // cli will go here
+      prompt.start();
+      prompt.message='';
+      const get=util.promisify(prompt.get);
+
+      const userSchema=[
+        {
+          description:'Enter username',
+          name:'username',
+          required:true,
+        },
+      ];
+
+      const {username}=await get(userSchema);
+
     } catch (err) {
       console.log(`Failed with ${err}`);
       process.exit(1);
